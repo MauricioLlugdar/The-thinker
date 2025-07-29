7be8beb0a7d83673ca8a8db80ed3268e6f33d840
Maurillugdar <maurillugdar@gmail.com>
2025-07-29 17:19:19 -0300

    FEAT: now is detected by Spring with RestController

--- src/main/java/com/maullu/the_thinker/IdeasController.java
+++ src/main/java/com/maullu/the_thinker/IdeasController.java
@@ -1,4 +1,10 @@
 package com.maullu.the_thinker;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ideas")
 public class IdeasController {
+
 }
