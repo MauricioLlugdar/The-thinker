7be5326e01291e1e758ae73992ba4ab1e4089537
Maurillugdar <maurillugdar@gmail.com>
2025-07-29 13:07:51 -0300

    Spring Initializr

---
+++ src/main/java/com/maullu/the_thinker/TheThinkerApplication.java
@@ -0,0 +1,13 @@
+package com.maullu.the_thinker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheThinkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheThinkerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maullu/the_thinker/TheThinkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maullu.the_thinker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheThinkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
