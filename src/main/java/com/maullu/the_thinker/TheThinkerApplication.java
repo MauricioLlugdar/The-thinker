dbcf0cb534727fc5b4d94eeb02c565deac2b6576
Maurillugdar <maurillugdar@gmail.com>
2025-07-29 16:48:12 -0300

    FEAT: add swagger

--- src/main/java/com/maullu/the_thinker/TheThinkerApplication.java
+++ src/main/java/com/maullu/the_thinker/TheThinkerApplication.java
@@ -2,8 +2,10 @@ package com.maullu.the_thinker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TheThinkerApplication {
 
 	public static void main(String[] args) {
