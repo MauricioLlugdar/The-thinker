d40ddeaf81844638d3f99427452c44ebf5017f94
Maurillugdar <maurillugdar@gmail.com>
2025-07-30 17:47:46 -0300

    FIX: not using the id of the user yet

--- src/main/java/com/maullu/the_thinker/Model/User.java
+++ src/main/java/com/maullu/the_thinker/Model/User.java
@@ -1,6 +1,4 @@
 package com.maullu.the_thinker.Model;
 
-import org.springframework.data.annotation.Id;
-
-public record User(@Id Long id, String name, String email, String Password) {
+public record User(Long id, String name, String email, String Password) {
 }
