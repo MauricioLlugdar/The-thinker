c6cde95b27402e82cfee39ac9c5dbe6188016f11
Maurillugdar <maurillugdar@gmail.com>
2025-07-30 17:49:19 -0300

    FIX: swagger not working with the vers of spring

--- src/main/java/com/maullu/the_thinker/TheThinkerApplication.java
+++ src/main/java/com/maullu/the_thinker/TheThinkerApplication.java
@@ -2,10 +2,8 @@ package com.maullu.the_thinker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class TheThinkerApplication {
 
 	public static void main(String[] args) {
