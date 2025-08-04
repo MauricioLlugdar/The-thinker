92fc08b6173fa71035dd0959159c2a2bbfa1b1b3
Maurillugdar <maurillugdar@gmail.com>
2025-08-04 02:17:34 -0300

    FEAT: enable paging and sorting on the responses

--- src/main/java/com/maullu/the_thinker/IdeasRepository.java
+++ src/main/java/com/maullu/the_thinker/IdeasRepository.java
@@ -3,9 +3,10 @@ package com.maullu.the_thinker;
 import com.maullu.the_thinker.Model.Idea;
 import com.maullu.the_thinker.Model.Visibility;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
 
-public interface IdeasRepository extends CrudRepository<Idea, Long> {
+public interface IdeasRepository extends CrudRepository<Idea, Long>, PagingAndSortingRepository<Idea, Long> {
     List<Idea> findByVisibility(Visibility visibility);
 }
