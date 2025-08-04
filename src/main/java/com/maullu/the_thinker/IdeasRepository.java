f968d54d460935fa2a70b0166fcd313aaf933a28
Maurillugdar <maurillugdar@gmail.com>
2025-08-04 02:41:14 -0300

    FEAT: Add paging in findByVisibility

--- src/main/java/com/maullu/the_thinker/IdeasRepository.java
+++ src/main/java/com/maullu/the_thinker/IdeasRepository.java
@@ -5,8 +5,9 @@ import com.maullu.the_thinker.Model.Visibility;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+import java.awt.print.Pageable;
 import java.util.List;
 
 public interface IdeasRepository extends CrudRepository<Idea, Long>, PagingAndSortingRepository<Idea, Long> {
-    List<Idea> findByVisibility(Visibility visibility);
+    List<Idea> findByVisibility(Visibility visibility, Pageable pageable);
 }
