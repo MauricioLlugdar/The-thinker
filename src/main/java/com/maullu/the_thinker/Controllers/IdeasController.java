5cd7056b43d27c5cb9c4ab0a3d8cd5d345408eaa
Maurillugdar <maurillugdar@gmail.com>
2025-08-07 16:05:45 -0300

    FEAT: create method implemented

--- src/main/java/com/maullu/the_thinker/Controllers/IdeasController.java
+++ src/main/java/com/maullu/the_thinker/Controllers/IdeasController.java
@@ -6,11 +6,10 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -44,5 +43,15 @@ public class IdeasController {
             return ResponseEntity.ok(idea.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> createIdea(@RequestBody Idea newIdea, UriComponentsBuilder ucb){
+        Idea savedIdea = ideasRepository.save(newIdea);
+        URI locationOfTheNewIdea = ucb
+                .path("/ideas/{id}")
+                .buildAndExpand(savedIdea.getId())
+                .toUri();
+        return ResponseEntity.created(locationOfTheNewIdea).build();
     }
 }
