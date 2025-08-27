9fb65401be76d6b207fdca4ce38c1db8d5a4cc43
Maurillugdar <maurillugdar@gmail.com>
2025-08-27 14:17:17 -0300

    FEAT: user model

--- src/main/java/com/maullu/the_thinker/Model/User.java
+++ src/main/java/com/maullu/the_thinker/Model/User.java
@@ -1,4 +1,52 @@
 package com.maullu.the_thinker.Model;
 
-public record User(Long id, String name, String email, String Password) {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
 }
