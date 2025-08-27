3f8dc12fae67756c8541f74df3c2a9281163a42a
Maurillugdar <maurillugdar@gmail.com>
2025-08-27 17:29:28 -0300

    FEAT: create user table

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,22 @@
+
+CREATE TABLE IF NOT EXISTS "user"
+(
+    user_id BIGSERIAL,
+    name VARCHAR(32) NOT NULL,
+    email VARCHAR(64) NOT NULL,
+    password VARCHAR(128) NOT NULL,
+    role VARCHAR(8) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
 CREATE TABLE IF NOT EXISTS idea
 (
-    id SERIAL,
+    id BIGSERIAL,
     title VARCHAR(64) NOT NULL,
     description VARCHAR(256),
     visibility VARCHAR(16) NOT NULL,
-    PRIMARY KEY (id)
+    owner_id BIGINT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (owner_id)
+        REFERENCES "user"(user_id)
 );
