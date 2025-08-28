8b0ea9e0849e553e4b82d7c39d8da584d7674417
Maurillugdar <maurillugdar@gmail.com>
2025-08-27 21:59:10 -0300

    TEST: user findById

--- src/test/java/com/maullu/the_thinker/UserIntegrationTests.java
+++ src/test/java/com/maullu/the_thinker/UserIntegrationTests.java
@@ -1,9 +1,42 @@
 package com.maullu.the_thinker;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import com.maullu.the_thinker.Model.Role;
+import com.maullu.the_thinker.Model.User;
 import com.maullu.the_thinker.Repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserIntegrationTests extends BaseIntegrationTest{
     @Autowired
     UserRepository userRepository;
+
+    User user1;
+    User user2;
+
+    @BeforeEach
+    void setup(){
+        userRepository.deleteAll();
+
+        User user1 = new User(null, "Carlos", "cgimenez@gmail.com", "123", Role.USER);
+        User user2 = new User(null, "Mauricio", "maurillugdar@gmail.com", "brime", Role.ADMIN);
+        this.user1 = userRepository.save(user1);
+        this.user2 = userRepository.save(user2);
+
+    }
+
+    @Test
+    void shouldReturnUserById(){
+        ResponseEntity<String> response = restTemplate
+                .getForEntity("/user/"+ user1.getId(), String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        assertThat(user1.getName()).isEqualTo(documentContext.read("$.name"));
+    }
 }
