74d89501ab79ce55a83da5765feec804f56e814a
Maurillugdar <maurillugdar@gmail.com>
2025-08-30 02:32:08 -0300

    FEAT: Create user controller endpoint

--- src/main/java/com/maullu/the_thinker/Controllers/UserController.java
+++ src/main/java/com/maullu/the_thinker/Controllers/UserController.java
@@ -4,11 +4,10 @@ import com.maullu.the_thinker.Model.Idea;
 import com.maullu.the_thinker.Model.User;
 import com.maullu.the_thinker.Repository.UserRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.Optional;
 
 @RestController
@@ -28,5 +27,15 @@ public class UserController {
             return ResponseEntity.ok().body(user.get());
         }
         return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    ResponseEntity<Void> createUser(@RequestBody User user, UriComponentsBuilder ucb){
+        User savedUser = userRepository.save(user);
+        URI locationUser = ucb
+                .path("/user/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+        return ResponseEntity.created(locationUser).build();
     }
 }
