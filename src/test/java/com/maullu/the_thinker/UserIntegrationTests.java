0b1d54538736bf0ea12b4b490ec8ce34ab9d624d
Maurillugdar <maurillugdar@gmail.com>
2025-08-30 02:39:56 -0300

    TEST: delete user

--- src/test/java/com/maullu/the_thinker/UserIntegrationTests.java
+++ src/test/java/com/maullu/the_thinker/UserIntegrationTests.java
@@ -8,8 +8,10 @@ import com.maullu.the_thinker.Repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.net.URI;
 
@@ -43,7 +45,7 @@ public class UserIntegrationTests extends BaseIntegrationTest{
         assertThat(user1.getName()).isEqualTo(documentContext.read("$.name"));
     }
 
-    @Test
+    @Test @DirtiesContext
     void shouldCreateUser(){
         User user = new User(null, "Mauri", "mauriciojllugdar@gmail.com", "123", Role.USER);
         ResponseEntity<Void> createResponse = restTemplate
@@ -59,5 +61,12 @@ public class UserIntegrationTests extends BaseIntegrationTest{
         System.out.println(documentContext);
         String name = documentContext.read("$.name");
         assertThat(name).isEqualTo(user.getName());
+    }
+
+    @Test @DirtiesContext
+    void shouldDeleteUser(){
+        ResponseEntity<Void> deleteResponse = restTemplate
+                .exchange("/user/"+user1.getId(), HttpMethod.DELETE, null,Void.class);
+        assertThat(deleteResponse.getStatusCode()).isEqualTo(HttpStatus.NO_CONTENT);
     }
 }
